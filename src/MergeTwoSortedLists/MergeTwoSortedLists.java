b55a1305ed59993a44d908f23b03ed74341400e3
audgnssweet <audgnssweet@naver.com>
2021-07-27 13:57:25 +0900

    두 정렬된 리스트 합치기

--- src/MergeTwoSortedLists/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists/MergeTwoSortedLists.java
@@ -1,10 +1,36 @@
 package MergeTwoSortedLists;
 
-import PalindromeLinkedList.ListNode;
-
 public class MergeTwoSortedLists {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
 
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+
+        ListNode result = l1.val <= l2.val ? l1 : l2;
+        ListNode temp1 = l1, temp2 = l2, temp = null;
+
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                while (temp1 != null && temp1.val <= temp2.val) {
+                    temp = temp1;
+                    temp1 = temp1.next;
+                }
+                temp.next = temp2;
+            } else {
+                while (temp2 != null && temp2.val <= temp1.val) {
+                    temp = temp2;
+                    temp2 = temp2.next;
+                }
+                temp.next = temp1;
+            }
+        }
+
+        return result;
     }
+
 }
