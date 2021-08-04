11e7b53569402a8fea772bcc5c0aa685499704b1
audgnssweet <audgnssweet@naver.com>
2021-08-04 13:20:33 +0900

    섬의 개수 구하기

---
+++ src/NumberOfIslands/NumberOfIslands.java
@@ -0,0 +1,37 @@
+package NumberOfIslands;
+
+public class NumberOfIslands {
+
+    public int numIslands(char[][] grid) {
+        int[] point = null;
+        int result = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, j, i);
+                    ++result;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private void dfs(char[][] grid, int x, int y) {
+        grid[y][x] = '0';
+
+        if (y > 0 && grid[y - 1][x] == '1') {
+            dfs(grid, x, y - 1);
+        }
+        if (x < grid[y].length - 1 && grid[y][x + 1] == '1') {
+            dfs(grid, x + 1, y);
+        }
+        if (y < grid.length - 1 && grid[y + 1][x] == '1') {
+            dfs(grid, x, y + 1);
+        }
+        if (x > 0 && grid[y][x - 1] == '1') {
+            dfs(grid, x - 1, y);
+        }
+    }
+}
