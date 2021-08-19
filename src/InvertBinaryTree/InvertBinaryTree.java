f7984c92bdbc28d05684eaeff9d90fb76970596c
audgnssweet <audgnssweet@naver.com>
2021-08-19 13:20:09 +0900

    이진 트리 반전

--- src/InvertBinaryTree/InvertBinaryTree.java
+++ src/InvertBinaryTree/InvertBinaryTree.java
@@ -2,6 +2,22 @@ package InvertBinaryTree;
 
 public class InvertBinaryTree {
     public TreeNode invertTree(TreeNode root) {
-        return null;
+        return dfs(root);
+    }
+
+    private TreeNode dfs(TreeNode node) {
+        if (node == null) {
+            return null;
+        }
+
+        dfs(node.left);
+        dfs(node.right);
+
+        TreeNode tmp;
+        tmp = node.left;
+        node.left = node.right;
+        node.right = tmp;
+
+        return node;
     }
 }
--- src/InvertBinaryTree/Readme.md
+++ src/InvertBinaryTree/Readme.md
@@ -6,8 +6,6 @@ https://leetcode.com/problems/invert-binary-tree
 
 <h5>내 풀이법</h5>
 
-
-<h5>정리</h5>
-
+    1. DFS를 이용한 재귀구조로 해결했다.
 
 ---
