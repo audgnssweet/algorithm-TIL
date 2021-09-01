0c6af48fb264e2fb96da4f81424c97fdee8c40db
audgnssweet <audgnssweet@naver.com>
2021-09-01 13:43:59 +0900

    유효한 애너그램

--- src/ValidAnagram/OtherValidAnagram.java
+++ src/ValidAnagram/OtherValidAnagram.java
@@ -1,2 +1,22 @@
-package ValidAnagram;public class OtherValidAnagram {
+package ValidAnagram;
+
+public class OtherValidAnagram {
+
+    public boolean isAnagram(String str1, String str2) {
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+        for (char c : str1.toCharArray()) {
+            count1[c - 'a']++;
+        }
+        for (char c : str2.toCharArray()) {
+            count2[c - 'a']++;
+        }
+
+        for (int i = 0; i <= 26; i++) {
+            if (count1[i] != count2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/ValidAnagram/ValidAnagram.java
+++ src/ValidAnagram/ValidAnagram.java
@@ -1,2 +1,26 @@
-package ValidAnagram;public class ValidAnagram {
+package ValidAnagram;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] chars1 = s.toCharArray();
+        char[] chars2 = t.toCharArray();
+
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+
+        for (int i = 0; i < chars1.length; i++) {
+            if (chars1[i] != chars2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
