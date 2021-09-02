3e7a9472c365167eda77baa40dd3b3c9b79b9141
audgnssweet <audgnssweet@naver.com>
2021-09-02 10:24:46 +0900

    이분?탐색

---
+++ src/BianrySearch/BianrySearch.java
@@ -0,0 +1,23 @@
+package BianrySearch;
+
+public class BianrySearch {
+
+    public int search(int[] nums, int target) {
+        return binSearch(nums, target, 0, nums.length - 1);
+    }
+
+    private int binSearch(int[] nums, int target, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = (start + end) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            return binSearch(nums, target, mid + 1, end);
+        } else {
+            return binSearch(nums, target, start, mid - 1);
+        }
+    }
+}
---
+++ src/BianrySearch/Readme.md
@@ -0,0 +1,8 @@
+https://leetcode.com/problems/binary-search
+
+---
+
+<h5>풀이</h5>
+
+    1. 배열로 binary search 구현
+    
