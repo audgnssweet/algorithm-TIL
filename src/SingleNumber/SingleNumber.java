cc14a104e96019589398e0dc8d4371f5a095f491
audgnssweet <audgnssweet@naver.com>
2021-09-03 02:08:00 +0900

    싱글 넘버

---
+++ src/SingleNumber/SingleNumber.java
@@ -0,0 +1,18 @@
+package SingleNumber;
+
+import java.util.Arrays;
+
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i += 2) {
+            if (!((i + 1) < nums.length) || nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+
+        return 0;
+    }
+}
