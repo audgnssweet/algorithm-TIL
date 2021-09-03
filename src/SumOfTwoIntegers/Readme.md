94850d0a627361c6edf8acd17e4d2f6377e1b4dc
audgnssweet <audgnssweet@naver.com>
2021-09-03 12:34:35 +0900

    비트 마스킹

---
+++ src/SumOfTwoIntegers/Readme.md
@@ -0,0 +1,13 @@
+## 두 정수의 합
+
+https://leetcode.com/problems/sum-of-two-integers/
+
+<h4> 풀이 </h4>
+
+    1. 
+
+---
+
+<h4> 비트 마스킹 </h4>
+
+    비트 마스킹은 1가지고 하는거다.
---
+++ src/SumOfTwoIntegers/SumOfTwoIntegers.java
@@ -0,0 +1,15 @@
+package SumOfTwoIntegers;
+
+public class SumOfTwoIntegers {
+
+    public int getSum(int a, int b) {
+        int mask = 0, up = 0;
+        int idx = 0;
+
+        while (idx < 32) {
+            
+
+            idx += 1;
+        }
+    }
+}
