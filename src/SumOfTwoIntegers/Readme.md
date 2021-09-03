7e50d8662d82c925910c2107668b6ce4e7c9df2f
audgnssweet <audgnssweet@naver.com>
2021-09-03 13:02:16 +0900

    두 정수의 합

--- src/SumOfTwoIntegers/Readme.md
+++ src/SumOfTwoIntegers/Readme.md
@@ -4,10 +4,7 @@ https://leetcode.com/problems/sum-of-two-integers/
 
 <h4> 풀이 </h4>
 
-    1. 
+    1. 비트 마스킹을 이용했다.
 
 ---
 
-<h4> 비트 마스킹 </h4>
-
-    비트 마스킹은 1가지고 하는거다.
--- src/SumOfTwoIntegers/SumOfTwoIntegers.java
+++ src/SumOfTwoIntegers/SumOfTwoIntegers.java
@@ -3,13 +3,38 @@ package SumOfTwoIntegers;
 public class SumOfTwoIntegers {
 
     public int getSum(int a, int b) {
-        int mask = 0, up = 0;
+        int up = 0;
         int idx = 0;
+        int res = 0;
 
         while (idx < 32) {
-            
+            int bitA = (a & (1 << idx));
+            int bitB = (b & (1 << idx));
+
+            if ((bitA == bitB) && (bitA != 0)) {
+                if (up == 1) {
+                    res |= (1 << idx);
+                }
+                up = 1;
+            }
+
+            if ((bitA == bitB) && (bitA == 0)) {
+                if (up == 1) {
+                    res |= (1 << idx);
+                    up = 0;
+                }
+            }
+
+            if (bitA != bitB) {
+                if (up != 1) {
+                    res |= (1 << idx);
+                }
+            }
 
             idx += 1;
         }
+
+        return res;
     }
+
 }
