9273a386dac4b309eca401117cf916b76eed5b5d
audgnssweet <audgnssweet@naver.com>
2021-10-09 22:45:01 +0900

    Hindex

---
+++ src/Hindex/Solution.java
@@ -0,0 +1,27 @@
+package Hindex;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static int solution(int[] citations) {
+        Arrays.sort(citations);
+
+        int res = citations[citations.length - 1];
+        while (res > 0) {
+            int idx = Arrays.binarySearch(citations, res);
+            idx = idx >= 0 ? idx : Math.abs(idx + 1);
+            if (citations.length - idx >= res) {
+                break;
+            }
+
+            res -= 1;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        solution(new int[]{3, 0, 6, 1, 5});
+    }
+}
