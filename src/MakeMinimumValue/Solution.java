ec8d495b5fcd60033bf3955d61342a2754bcc469
audgnssweet <audgnssweet@naver.com>
2021-10-10 23:18:09 +0900

    최소값 만들기

---
+++ src/MakeMinimumValue/Solution.java
@@ -0,0 +1,18 @@
+package MakeMinimumValue;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int res = 0;
+        for (int i = 0; i < A.length; ++i) {
+            res += (A[i] * B[B.length - 1 - i]);
+        }
+
+        return res;
+    }
+}
