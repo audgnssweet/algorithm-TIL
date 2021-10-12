eddbcdc474e1d2edb321d27f376ef0366aa919bd
audgnssweet <audgnssweet@naver.com>
2021-10-12 11:11:20 +0900

    카카오 공채 파일이름 정렬

--- src/SortFileNames/OtherSolution.java
+++ src/SortFileNames/OtherSolution.java
@@ -1,9 +1,34 @@
 package SortFileNames;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class OtherSolution {
 
+    static class FileName {
+
+        String head;
+        String number;
+        String tail;
+
+        public FileName(String head, String number, String tail) {
+            this.head = head;
+            this.number = number;
+            this.tail = tail;
+        }
+    }
+
     public static void main(String[] args) {
-        if (m.find()){
+        Matcher m = Pattern.compile("").matcher("");
+        String number = "";
+        String s = "";
+        String head = "";
+        String tail = "";
+        List<FileName> fileNameList = new ArrayList<>();
+
+        if (m.find()) {
             number = m.group();
             int numberStartIndex = s.indexOf(number);
             head = s.substring(0, numberStartIndex);
