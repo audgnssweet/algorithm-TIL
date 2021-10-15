eb189ed8c8666e1885b6af0f48240ac0ee7b8544
audgnssweet <audgnssweet@naver.com>
2021-10-15 10:53:09 +0900

    JadenCase 문자열 만들기

---
+++ src/MakeJadenCase/Readme.md
@@ -0,0 +1,4 @@
+## JadenCase 문자열 만들기
+
+    1. 문자열을 split할 때 ""로 split하면 단위는 1글자단위, toCharArray() 처럼
+    끊겨서 들어가지만 String형으로 들어간다.
---
+++ src/MakeJadenCase/Solution.java
@@ -0,0 +1,17 @@
+package MakeJadenCase;
+
+public class Solution {
+
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        String[] sp = s.toLowerCase().split("");
+        boolean flag = true;
+
+        for (String ss : sp) {
+            answer.append(flag ? ss.toUpperCase() : ss);
+            flag = ss.equals(" ");
+        }
+
+        return answer.toString();
+    }
+}
