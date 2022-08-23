9a236fd810563e60902cefe692a8a242fcd5f864
audgnssweet <audgnssweet@naver.com>
2022-08-23 12:06:38 +0900

    카카오 양궁 게임

---
+++ src/kakao2022intern/Solution2.java
@@ -0,0 +1,133 @@
+package kakao2022intern;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Solution2 {
+
+	public static int standardTime;
+	public static int standardFee;
+	public static int timeUnit;
+	public static int feeUnit;
+
+	public int[] solution(int[] fees, String[] records) {
+		standardTime = fees[0];
+		standardFee = fees[1];
+		timeUnit = fees[2];
+		feeUnit = fees[3];
+
+		Parking parking = new Parking();
+
+		Arrays.stream(records)
+			.map(this::getTicket)
+			.forEach(parking::process);
+
+		return parking.getResultFees();
+	}
+
+	private Ticket getTicket(String s) {
+		String[] split = s.split(" ");
+		return new Ticket(split[1], split[0]);
+	}
+
+	static class Parking {
+
+		private Map<String, Ticket> history = new HashMap<>();
+		private Map<String, Integer> durationByCars = new HashMap<>();
+
+		public void process(Ticket ticket) {
+			if (alreadyIn(ticket)) {
+				Ticket in = getInTicket(ticket);
+				int duration = in.getDuration(ticket);
+				addDuration(ticket, duration);
+			} else {
+				addHistory(ticket);
+			}
+		}
+
+		public int[] getResultFees() {
+			processRemainTickets();
+
+			return durationByCars.entrySet().stream()
+				.sorted(Map.Entry.comparingByKey())
+				.mapToInt(Map.Entry::getValue)
+				.map(this::getFee)
+				.toArray();
+		}
+
+		private void processRemainTickets() {
+			List<Ticket> outTickets = history.values().stream()
+				.map(Ticket::getDefaultOutTicket)
+				.collect(Collectors.toList());
+
+			outTickets.forEach(this::process);
+		}
+
+		private int getFee(int duration) {
+			duration -= standardTime;
+			int fee = standardFee;
+
+			while (duration > 0) {
+				fee += feeUnit;
+				duration -= timeUnit;
+			}
+
+			return fee;
+		}
+
+		private boolean alreadyIn(Ticket ticket) {
+			return history.containsKey(ticket.getCarNum());
+		}
+
+		private Ticket getInTicket(Ticket ticket) {
+			return history.remove(ticket.getCarNum());
+		}
+
+		private void addDuration(Ticket ticket, int duration) {
+			durationByCars.put(ticket.getCarNum(), getDurationSum(ticket) + duration);
+		}
+
+		private Integer getDurationSum(Ticket ticket) {
+			return durationByCars.getOrDefault(ticket.getCarNum(), 0);
+		}
+
+		private void addHistory(Ticket ticket) {
+			history.put(ticket.getCarNum(), ticket);
+		}
+	}
+
+	static class Ticket {
+		private static final String DELIM = ":";
+
+		private String carNum;
+		private String time;
+
+		public Ticket(String carNum, String time) {
+			this.carNum = carNum;
+			this.time = time;
+		}
+
+		public String getCarNum() {
+			return this.carNum;
+		}
+
+		public int getTime() {
+			String[] timeMinute = this.time.split(DELIM);
+			int hour = Integer.parseInt(timeMinute[0]);
+			int minute = Integer.parseInt(timeMinute[1]);
+
+			return hour * 60 + minute;
+		}
+
+		public int getDuration(Ticket out) {
+			return out.getTime() - this.getTime();
+		}
+
+		public Ticket getDefaultOutTicket() {
+			return new Ticket(this.carNum, "23:59");
+		}
+	}
+}
---
+++ src/kakao2022intern/Solution3.java
@@ -0,0 +1,134 @@
+package kakao2022intern;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Solution3 {
+
+	static int[] apeach;
+	static int[] ryan = new int[11];
+	static int scoreGapMax = 0;
+	static PriorityQueue<int[]> winPlans = new PriorityQueue<>((p1, p2) -> {
+		for (int i = p1.length - 1; i >= 0; i--) {
+			if (p1[i] > p2[i]) {
+				return -1;
+			} else if (p2[i] > p1[i]) {
+				return 1;
+			}
+		}
+		return 0;
+	});
+
+	public static void main(String[] args) {
+		Solution3 s = new Solution3();
+		int[] res = s.solution(9, new int[] {0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1});
+		System.out.println(Arrays.toString(res));
+	}
+
+	public int[] solution(int n, int[] info) {
+		apeach = info;
+		play(0, n);
+		if (winPlans.isEmpty()) {
+			return new int[] {-1};
+		}
+		return winPlans.poll();
+	}
+
+	void play(int depth, int arrow) {
+		if (isEnd(depth, arrow)) {
+			if (isLionWin()) {
+				renewWinPlan(arrow);
+			}
+		} else {
+			if (canShot(depth, arrow)) {
+				shotAndPlay(depth, arrow);
+			}
+			noShotAndPlay(depth, arrow);
+		}
+	}
+
+	void shotAndPlay(int depth, int arrow) {
+		int needArrow = apeach[depth] + 1;
+		ryan[depth] = needArrow;
+		play(depth + 1, arrow - needArrow);
+		ryan[depth] = 0;
+	}
+
+	void noShotAndPlay(int depth, int arrow) {
+		play(depth + 1, arrow);
+	}
+
+	boolean canShot(int target, int arrow) {
+		return apeach[target] < arrow;
+	}
+
+	boolean isEnd(int depth, int arrow) {
+		return depth == apeach.length || arrow <= 0;
+	}
+
+	boolean isLionWin() {
+		int[] scores = getScores();
+		int apeachScore = scores[0];
+		int ryanScore = scores[1];
+
+		return ryanScore > apeachScore;
+	}
+
+	void renewWinPlan(int arrow) {
+		int scoreGap = getScoreGap();
+		if (scoreGap > scoreGapMax) {
+			winPlans.clear();
+			scoreGapMax = scoreGap;
+		}
+		if (scoreGap == scoreGapMax) {
+			addWinPlan(arrow);
+		}
+	}
+
+	void addWinPlan(int arrow) {
+		if (arrow > 0) {
+			ryan[ryan.length - 1] = arrow;
+			winPlans.offer(Arrays.copyOf(ryan, ryan.length));
+			ryan[ryan.length - 1] = 0;
+		}
+		winPlans.offer(Arrays.copyOf(ryan, ryan.length));
+	}
+
+	int getScoreGap() {
+		int[] scores = getScores();
+		int apeachScore = scores[0];
+		int ryanScore = scores[1];
+
+		return ryanScore - apeachScore;
+	}
+
+	int[] getScores() {
+		int apeachScore = 0;
+		int ryanScore = 0;
+		for (int i = 0; i < apeach.length; i++) {
+			if (isApeachWin(i)) {
+				apeachScore += getScore(i);
+			} else if (isRyanWin(i)) {
+				ryanScore += getScore(i);
+			}
+		}
+
+		return new int[] {apeachScore, ryanScore};
+	}
+
+	int getScore(int i) {
+		return 10 - i;
+	}
+
+	boolean isApeachWin(int target) {
+		return shot(apeach[target]) && apeach[target] >= ryan[target];
+	}
+
+	boolean isRyanWin(int target) {
+		return shot(ryan[target]) && ryan[target] > apeach[target];
+	}
+
+	boolean shot(int arrowCnt) {
+		return arrowCnt > 0;
+	}
+}
